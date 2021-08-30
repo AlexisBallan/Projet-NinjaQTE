0d0734e5657b2be01f9715bb66d6c90cdc760fae
Alexis <allexis.bal@hotmail.fr>
2021-08-30 15:18:47 -0400

    Add Cible
    J'ai ajouté des cibles et commencer à faire fonctionner le système de point

---
+++ Projet-NinjaQTE/Assets/Scripts/Point.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Point : MonoBehaviour
+{
+    public int NbPoint = 0;
+
+    private void Update()
+    {
+        Debug.Log(NbPoint);
+        
+    }
+
+    public void addPoint()
+    {
+        NbPoint++;
+    }
+}
---
+++ Projet-NinjaQTE/Assets/Scripts/ToucherCible.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToucherCible : MonoBehaviour
+{
+    public GameObject Point;
+
+    private void OnMouseEnter()
+    {
+        Point.GetComponent<Point>()?.addPoint();
+        Destroy(gameObject);
+    }
+}
